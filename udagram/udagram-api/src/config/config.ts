ebc8524aa2e3ef5a2d67ec31d4b7d4eb7dfbb5d0
youmna3 <youmna54@gmail.com>
2022-05-15 07:20:51 +0200

    edit the env and config file

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -17,6 +17,8 @@ export const config = {
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
+  aws_acess_key_id: process.env.AWS_ACCESS_KEY_ID,
+  aws_secret_acess_key: process.env.AWS_SECRET_ACCESS_KEY,
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
