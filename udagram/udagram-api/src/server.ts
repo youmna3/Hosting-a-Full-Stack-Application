8a8be3d0608d562c7b6345886fdd2d05af2f1dbb
youmna3 <youmna54@gmail.com>
2022-05-17 05:33:03 +0200

    edit beanstalk and server.ts

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -28,7 +28,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Root URI call
   app.get("/", async (req, res) => {
-    res.send("/api/v0/");
+    res.send("hello");
   });
 
   // Start the Server
